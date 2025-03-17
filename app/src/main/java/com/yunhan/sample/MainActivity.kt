c3779afa1d848170cf0dc2dea8dbc626bc3b3afe
yunhan <dbsgks17@naver.com>
2025-03-17 09:57:03 +0900

    Fix: [app] remove enableEdgeToEdge

--- app/src/main/java/com/yunhan/sample/MainActivity.kt
+++ app/src/main/java/com/yunhan/sample/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.yunhan.sample
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.yunhan.sample.ui.SampleApp
 import com.yunhan.sample.ui.theme.SampleTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,7 +17,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SampleTheme {
                 SampleApp()
--- app/src/main/java/com/yunhan/sample/ui/SampleApp.kt
+++ app/src/main/java/com/yunhan/sample/ui/SampleApp.kt
@@ -2,7 +2,6 @@ package com.yunhan.sample.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
