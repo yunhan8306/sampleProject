507dd66707024530d35206843916dc0a27138e49
yunhan <dbsgks17@naver.com>
2025-03-18 14:48:45 +0900

    Feat: [presentation-designsystem] CommonLoadingDialog

---
+++ presentation/designsystem/src/main/java/com/yunhan/presentation/designsystem/component/loading/CommonLoadingDialog.kt
@@ -0,0 +1,60 @@
+package com.yunhan.presentation.designsystem.component.loading
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun CommonLoadingDialog(
+    isLoading: Boolean,
+    onDismiss: () -> Unit
+) {
+    if (isLoading) {
+        Dialog(onDismissRequest = onDismiss) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                color = MaterialTheme.colorScheme.surface,
+                tonalElevation = 8.dp
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .wrapContentSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(48.dp),
+                        strokeWidth = 4.dp
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "Loading...",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CommonLoadingDialogPreView() {
+    CommonLoadingDialog(
+        isLoading = true,
+        onDismiss = {}
+    )
+}
