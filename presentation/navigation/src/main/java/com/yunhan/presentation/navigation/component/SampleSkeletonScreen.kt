a4660559e57a6439e02aff243702dd1f319a484e
yunhan <dbsgks17@naver.com>
2025-03-20 09:19:25 +0900

    Fix: [presentation-navigation] SampleSkeletonScreen color

--- presentation/navigation/src/main/java/com/yunhan/presentation/navigation/component/SampleSkeletonScreen.kt
+++ presentation/navigation/src/main/java/com/yunhan/presentation/navigation/component/SampleSkeletonScreen.kt
@@ -1,13 +1,16 @@
 package com.yunhan.presentation.navigation.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.yunhan.presentation.designsystem.component.shimmer.CommonShimmerItem
 
@@ -20,15 +23,15 @@ fun SampleSkeletonScreen(
     ) {
         Spacer(modifier = Modifier.height(20.dp))
         CommonShimmerItem(
-            modifier = Modifier.fillMaxWidth().height(100.dp)
+            modifier = Modifier.background(color = Color.LightGray, shape = RoundedCornerShape(8.dp)).fillMaxWidth().height(100.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
         CommonShimmerItem(
-            modifier = Modifier.fillMaxWidth().height(400.dp)
+            modifier = Modifier.background(color = Color.LightGray, shape = RoundedCornerShape(8.dp)).fillMaxWidth().height(400.dp)
         )
         Spacer(modifier = Modifier.height(20.dp))
         CommonShimmerItem(
-            modifier = Modifier.fillMaxWidth().height(300.dp)
+            modifier = Modifier.background(color = Color.LightGray, shape = RoundedCornerShape(8.dp)).fillMaxWidth().height(300.dp)
         )
     }
 }
